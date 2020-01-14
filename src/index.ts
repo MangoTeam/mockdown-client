cc13a6a6625764903d0b9a432455d94b49a38be7
Dylan Lukes <lukes.dylan@gmail.com>
2020-01-13 16:22:17 -0800

    remove ES6 Promise polyfill, shouldn't be necessary anymore?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Polyfill ES6 promises if they aren't available natively.
-import { polyfill } from 'es6-promise';
-polyfill();
+// import { polyfill } from 'es6-promise';
+// polyfill();
 
 // Polyfill the `fetch` global function for Node.
 import 'isomorphic-fetch';
